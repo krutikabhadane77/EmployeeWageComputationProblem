121f88b0ea461eaa5e2e8d75f6e705ec25f6366e
krutikabhadane77 <krutikabhadane588@gmail.com>
2022-10-28 21:17:59 +0530

    Added: UC-3 Add part time employee and wage

--- src/com/employeewagecomputationproblem/EmployeeWageComputation.java
+++ src/com/employeewagecomputationproblem/EmployeeWageComputation.java
@@ -1,24 +1,31 @@
 //UC1 Check employee is present or absent
 //UC2 Calculate daily employee wage
+//UC3 Add part time employee & wage
 package com.employeewagecomputationproblem;
 
 public class EmployeeWageComputation {
     public static void main(String args[])
     {
-        final int FULL_TIME = 1;
+        int FULL_TIME = 2;
+        int PART_TIME = 1;
         int WAGE_PER_HR = 20;
         int workingHours = 0;
-        double empType = Math.floor(Math.random() * 10) % 2;
+        double empType = Math.floor(Math.random() * 10) % 3;
         if (empType == FULL_TIME)
         {
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Present Full Time");
             workingHours = 8;
-        } else
+        }
+        else if (empType == PART_TIME)
+        {
+            System.out.println("Employee is Present Part Time");
+            workingHours = 4;
+        }
+        else
         {
             System.out.println("Employee is Absent");
         }
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
     }
-
 }
